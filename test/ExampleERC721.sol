5380e86a0dd2b0ab04dae5d0b576f63c47ef4ec0
Yigit Duman <yigitq@gmail.com>
2024-11-10 18:26:21 +0300

    update license and solidity version

--- test/ExampleERC721.sol
+++ test/ExampleERC721.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.25;
+pragma solidity >=0.8.27;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { IHonestWork } from "../src/IHonestWork.sol";
