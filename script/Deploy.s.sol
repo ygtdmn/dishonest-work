7404adba9fea3f9904086f98df65826ff50950b9
Yigit Duman <yigitq@gmail.com>
2024-11-10 18:26:56 +0300

    separate deployment scripts for sepolia and mainnet

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -9,8 +9,19 @@ import { IHonestWork } from "../src/IHonestWork.sol";
 
 contract Deploy is BaseScript {
     function run() public broadcast returns (DishonestWork dishonestWork) {
+        return runOnSepolia();
+    }
+
+    function runOnMainnet() public returns (DishonestWork dishonestWork) {
         address _0xfff = 0x8C9A4427e991c6485e559E3c4F79a88128d8be3E;
         IHonestWork honestWork = IHonestWork(0xCfED1cC741F68AF4778c2Eb8efDcFc0F9ab28466);
+        address balancerVault = address(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
+        dishonestWork = new DishonestWork(honestWork, _0xfff, balancerVault);
+    }
+
+    function runOnSepolia() public returns (DishonestWork dishonestWork) {
+        address _0xfff = 0x8C9A4427e991c6485e559E3c4F79a88128d8be3E;
+        IHonestWork honestWork = IHonestWork(0x47f62429558cFfB91dbD1edF4f1e94F151091fb9);
         address balancerVault = address(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
         dishonestWork = new DishonestWork(honestWork, _0xfff, balancerVault);
     }
